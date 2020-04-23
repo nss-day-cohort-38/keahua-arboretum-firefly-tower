9872c3255951f6d81b9cfc825002bfdb089c2944
sncandiani <sofiancandiani@gmail.com>
2020-04-23 16:02:10 -0500

    lists all animals in release animal

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -1,19 +1,43 @@
-from animals import RiverDolphin
+from animals import GoldDustDayGecko, HawaiianHappyFaceSpider, Kikakapu, NeneGoose, Opeapea, Pueo, RiverDolphin, Ulae
 
 def release_animal(arboretum):
     animal = None
 
-    print("1. River Dolphin")
-    print("2. Dragonfly")
+    print('1. Gold Dust Day Gecko')
+    print('2. RiverDolphin')
+    print('3. Nene Goose')
+    print('4. Kīkākapu')
+    print('5. Pueo')
+    print("6. 'Ulae")
+    print("7. Ope'ape'a")
+    print("8. Happy-Face Spider")
 
     choice = input("Choose animal to release > ")
 
     if choice == "1":
-        animal = RiverDolphin()
+        animal = GoldDustDayGecko()
 
     if choice == "2":
-        pass
+        animal = RiverDolphin()
+    
+    if choice == "3":
+        animal = NeneGoose()
+    
+    if choice == "4":
+        animal = Kikakapu()
+    
+    if choice == "5":
+        animal = Pueo()
 
+    if choice == "6":
+        animal = Ulae()
+
+    if choice == "7":
+        animal = Opeapea() 
+    
+    if choice == "8":
+        animal = HawaiianHappyFaceSpider()
+    
 
     for index, river in enumerate(arboretum.rivers):
         print(f'{index + 1}. River {river.id}')
