2d325699bc62cc48b9cf3957f17b1ed0908fac66
keithrpotempa <keithrpotempa@gmail.com>
2020-04-23 13:34:34 -0500

    working on tester

--- tester.py
+++ tester.py
@@ -2,20 +2,31 @@
 
 from animals import Animal, GoldDustDayGecko, HawaiianHappyFaceSpider, Kikakapu, NeneGoose, Opeapea, Pueo, RiverDolphin, Ulae
 from plants import RainbowEucalyptusTree, Silversword, MountainAppleTree, BlueJadeVine
+# TODO: add all biomes
+from environments import River
 
 animal_classes = [GoldDustDayGecko, HawaiianHappyFaceSpider, Kikakapu, NeneGoose, Opeapea, Pueo, RiverDolphin, Ulae]
 plant_classes = [RainbowEucalyptusTree, Silversword, MountainAppleTree, BlueJadeVine]
+environment_classes = [River]
 
 names = ["Liam", "Emma", "Noah", "Olivia", "William", "Ava", "James", "Isabella", "Oliver", "Sophia", "Benjamin", "Charlotte"]  
 
-#Print all of the attributes of an instance
-def printAttrs(instance):
+def mass_add_to_environs(class_list):
+    for environment_class in environment_classes:
+      print()
+      #Get an index
+      i = environment_class.index(environment_class)
+      #Use the index to claim a name
+      name = names[i]
+
+
+#Loop through a list of classes, initialize instances of them, and then print all that instances attributes
+def printAllClassesAttributes(class_list):
+  def printAttrs(instance):
     for attr in dir(instance):
         if(attr[:2] != "__"): # don't print anything starting with dunderscore
             print(attr, getattr(instance, attr))
-
-#Loop through a list of classes
-def classLoop(class_list):
+  
   for clss in class_list:
       print()
       #Get an index
@@ -34,4 +45,4 @@ def classLoop(class_list):
 
 #Call the function and pass in class list        
 # classLoop(plant_classes)
-classLoop(animal_classes)
+# classLoop(animal_classes)
