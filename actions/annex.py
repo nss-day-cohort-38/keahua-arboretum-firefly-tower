4334376b5cc4e79011114b104b523fd3c3158495
Matt Crook <matt.crook11@gmail.com>
2020-04-23 15:30:05 -0500

    annex and release animal menu options functionality.

--- actions/annex.py
+++ actions/annex.py
@@ -1,5 +1,11 @@
 import os
 from environments import River
+from environments import Swamp
+from environments import Coastline
+from environments import Grassland
+from environments import Mountain
+from environments import Forest
+
 
 def annex_habitat(arboretum):
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -7,11 +13,30 @@ def annex_habitat(arboretum):
     print("2. Swamp")
     print("3. Coastline")
     print("4. Grassland")
-
+    print("5. Mountain")
+    print("6. Forest")
     choice = input("Choose your habitat > ")
-
     if choice == "1":
         river = River()
         arboretum.rivers.append(river)
+        pass
     if choice == "2":
+        swamp = Swamp()
+        arboretum.rivers.append(swamp)
+        pass
+    if choice == "3":
+        coastline = Coastline()
+        arboretum.coastline.append(coastline)
+        pass
+    if choice == "4":
+        grassland = Grassland()
+        arboretum.grassland.append(grassland)
+        pass
+    if choice == "5":
+        mountain = Mountain()
+        arboretum.mountain.append(mountain)
+        pass
+    if choice == "6":
+        forest = Forest()
+        arboretum.forest.append(forest)
         pass
--- actions/release_animal.py
+++ actions/release_animal.py
@@ -1,26 +1,54 @@
 from animals import RiverDolphin
+from animals import GoldDustDayGecko
+from animals import HawaiianHappyFaceSpider
+from animals import Kikakapu
+from animals import NeneGoose
+from animals import Opeapea
+from animals import Ulae
+from animals import Pueo
+
 
 def release_animal(arboretum):
     animal = None
-
     print("1. River Dolphin")
-    print("2. Dragonfly")
+    print("2. Gold Dust Day Gecko")
+    print("3. Hawaiian Happy Face Spider")
+    print("4. Kikakapu")
+    print("5. Nene Goose")
+    print("6. Ope Ape A")
+    print("7. Pueo")
+    print("8. Ulae")
+
 
     choice = input("Choose animal to release > ")
+
 
     if choice == "1":
         animal = RiverDolphin()
 
     if choice == "2":
-        pass
+        animal = GoldDustDayGecko()
 
+    if choice == "3":
+        animal = HawaiianHappyFaceSpider()
+
+    if choice == "4":
+        animal = Kikakapu()
+
+    if choice == "5":
+        animal = NeneGoose()
+
+    if choice == "6":
+        animal = Opeapea()
+
+    if choice == "7":
+        animal = Pueo()
+
+    if choice == "8":
+        animal = Ulae()
 
     for index, river in enumerate(arboretum.rivers):
         print(f'{index + 1}. River {river.id}')
-
     print("Release the animal into which biome?")
     choice = input("> ")
-
     arboretum.rivers[int(choice) - 1].animals.append(animal)
-
-
