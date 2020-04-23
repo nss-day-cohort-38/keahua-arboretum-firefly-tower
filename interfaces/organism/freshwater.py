e8a877752a527bc9be25ab30ec72a2f5a4e0ad79
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-04-23 10:31:21 -0500

    put getters on parent class instead of children classes

--- interfaces/organism/freshwater.py
+++ interfaces/organism/freshwater.py
@@ -4,6 +4,6 @@ class IFreshwater(IAquatic):
 
     def __init__(self):
         super().__init__()
-        self.tolerate_fresh = True
+        self.freshwater = True
         self.tolerate_stagnant = False
         self.tolerate_current = True
