821505de90dc1931a8a9719cbf8ae19e75450990
keithrpotempa <keithrpotempa@gmail.com>
2020-04-23 12:10:18 -0500

    added identifiable init to plant class

--- plants/plant.py
+++ plants/plant.py
@@ -3,6 +3,7 @@ from interfaces import Identifiable
 class Plant(Identifiable):
 
     def __init__(self):
+      super().__init__()
       self.species = ""
       
       # Was in the original code but doesn't appear to be used
