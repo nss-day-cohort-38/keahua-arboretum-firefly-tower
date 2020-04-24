b6226693011aec97636f1b8bd042c6136fab0e2e
keithrpotempa <keithrpotempa@gmail.com>
2020-04-24 10:53:24 -0500

    got the first step of the plant menu working without duck typing

--- actions/add_plant.py
+++ actions/add_plant.py
@@ -7,32 +7,39 @@ def add_plant(arboretum):
     print("3. Rainbow Eucalyptus Tree")
     print("4. Blue Jade Vine")
     
-    def get_environ_instances(environ):
-        environ_instances = arboretum.environ 
+    def create_environ_options(instance_list, *args): 
         i = 1
-        for environ_instance in environ_instances:
-            print(f"{i}. {environ_instance}")
+        
+        for instance in instance_list:
+            print(f"{i}. {instance}")
             i += 1
+        
+        #If there is a second environment,
+        #continue the list of options
+        if len(args) > 0:
+            #Note: args in this instance returns a single list within a tuple
+            #so we're using this index to get that single list
+            for instance in args[0]:
+                print(f"{i}. {instance}")
+                i+= 1
             
-        new_choice = input("> ")
+        new_choice = input("Choose environment to plant in > ")
 
     choice = input("Choose plant to cultivate > ")
 
-    def what_environment(environment):
-        pass
-
     if choice == "1":
-        #TODO: fix this to be duck-typed        
+        #TODO: fix this to be duck-typed
+        create_environ_options(arboretum.mountains)        
         pass
       
     if choice == "2":
         #TODO: fix this to be duck-typed
-        return Arboretum.rivers
+        create_environ_options(arboretum.grasslands)
       
     if choice == "3":
         #TODO: fix this to be duck-typed
-        return Arboretum.swamps
+        create_environ_options(arboretum.forest)
     
     if choice == "4":
         #TODO: fix this to be duck-typed
-        return Arboretum.forest
+        create_environ_options(arboretum.forests, arboretum.swamps)
