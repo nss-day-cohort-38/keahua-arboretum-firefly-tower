a5f3d21970eb16132efcd604438caa9751a12688
keithrpotempa <keithrpotempa@gmail.com>
2020-04-27 09:54:51 -0500

    centering header text

--- header.py
+++ header.py
@@ -1,10 +1,13 @@
 import os
 
 def header(text):
-    os.get_terminal_size()
+    # Centering print text:
+    # https://stackoverflow.com/questions/33594958/is-it-possible-to-align-a-print-statement-to-the-center-in-python
+    def center (str):
+      #os.get_terminal_size().columns
+      return str.center(50)
     
-  
-    print("+-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+")
-    print(f"{text}")
-    print("+-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+")
+    print(center("+-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+"))
+    print(center(f"{text}"))
+    print(center("+-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+"))
     print()
